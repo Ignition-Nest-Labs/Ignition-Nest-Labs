d767b6303c5757c1292c1aeb374e43bea4a8f4b5
Karan <karanchandravanshi217@gmail.com>
2024-02-25 13:43:48 +0530

    Done

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,13 @@ import Navbar from "@/components/Navbar";
 import Story from "@/components/Story";
 import Teams from "@/components/Teams";
 import { Analytics } from "@vercel/analytics/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
    <>
    <Analytics/>
+   <SpeedInsights/>
    <div className="min-h-screen">
     <Navbar/>
     <Intro/>
